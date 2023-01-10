e155badefaa5bb60d3aa33fab77a17159e741cb3
sjsjsjsue <esther106@naver.com>
2023-01-10 15:43:18 +0900

    fix : s3에 파일 업로드

--- src/main/java/com/dw/member/controller/UploadController.java
+++ src/main/java/com/dw/member/controller/UploadController.java
@@ -1,13 +1,19 @@
 package com.dw.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.dw.member.service.UploadService;
+
 @RestController
 public class UploadController {
-    
+  
+  @Autowired
+  private UploadService uploadService;
+  
     @PostMapping("/api/v1/image")
     public boolean uploadImage(@RequestParam("image") MultipartFile[] locationFile){
       // MultipartFile[] : 클래스로 사진을 전송 받음.
@@ -17,7 +23,7 @@ public class UploadController {
 
 
         
-        return true;
+        return uploadService.uploadImageToAwsS3(locationFile);
     }
 
 }
--- src/main/java/com/dw/member/service/UploadService.java
+++ src/main/java/com/dw/member/service/UploadService.java
@@ -18,9 +18,13 @@ public class UploadService {
         String imageBasePath="https://s3이름.s3.ap-northeast-2.amazonaws.com";//디비에 저장할 때 사용
         String imagePath = "/upload/"+today; // 이미지를 저장할 폴더 
         //today : 날짜별로 저장.
-        String imageName = locationFile[0].getName()+".jpg"; // 이미지 저장할 사진 이름.
+        // 실무에서는 사진이름을 현재시각(초 까지) 혹은 pk 이름으로 저장한다.
+        // 사진 용량도 코딩으로 줄여서 저장한다.(image resize)
+        String imageName = "Sue"+locationFile[0].getName()+".jpg"; // 이미지 저장할 사진 이름.
         try{
-            awsService.uploadObject(locationFile[0],imagePath,imageName);
+            awsService.uploadObject(locationFile[0],imagePath,imageName); // s3 에 이미지 업로드
+            String s3Url = imageBasePath + imagePath +"/"+imageName;
+            //DB에 s3Url를 저장하는 로직을 구현하면 끝!
         }catch(Exception e){
             return false;
         }
